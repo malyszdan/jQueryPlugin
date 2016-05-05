797100e3511f68048173f3b016ecce93fb1f8169
Daniel Malyszczuk <malyszczuk.daniel@gmail.com>
2016-05-05 19:53:59 +0200

    add validate function

--- jqplug.js
+++ jqplug.js
@@ -1,28 +1,40 @@
 (function($) {
 
-	$.fn.checkNames = function(minLen) {
-		var pattern;
-		pattern = new RegExp("^[A-ZĄĆĘŁŃÓŚŹŻ]{1}[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]*$");
-		this.each(function() {
-			if(!pattern.test($(this).val()) || $(this).val().length <= minLen) {
+	$.fn.validate = function(params){
+		var pattern, minLen;
+		if(params.length == 0){
+        	var settings = $.extend({
+            	pattern : "^[A-ZĄĆĘŁŃÓŚŹŻ]{1}[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]*$",
+                minLen: 3
+               	 		}, params);
+        }else if(params.pattern ==='undefined'){
+        	var settings = $.extend({
+            	pattern : "^[A-ZĄĆĘŁŃÓŚŹŻ]{1}[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]*$"
+            }, params);
+        }else if(params.minLen==='undefined'){
+         	var settings = $.extend({
+            	minLen: 3 
+               	 		}, params);
+        }
+        pattern = new RegExp(params.pattern);
+        minLen = params.minLen;
+                
+        return this.each(function() {
+              if(!pattern.test($(this).val()) || $(this).val().length <= minLen) {
 					$(this).addClass('err');
 					console.log("blad");
 					$(":button").attr('disabled', true);	
-					
-					
 			} else {
 				if($(this).hasClass('err')){
 					$(this).removeClass('err');	
-					$(":button").attr('disabled', false);	
-					
-					
-				}
-		}});
+					$(":button").attr('disabled', false);}
+              		console.log("Sukces!");
+              }
+            });
+        };
+}		
 		
-	return this;
-	}
-
-})(jQuery);
+)(jQuery);
 
 
 
