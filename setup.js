36f1a0c7804cec77f132ab8ac68787bf362a1e3d
Daniel Malyszczuk <malyszczuk.daniel@gmail.com>
2016-05-05 11:29:31 +0200

    add setup script

---
+++ setup.js
@@ -0,0 +1,34 @@
+$(document).ready(function (){
+
+	$("#submit").click(function() {
+		validateForm($(this));
+	});
+
+	function validateForm() {
+		$(":text").checkNames({minLen : 3});
+		//$(":email").checkEmail();
+	}
+		
+
+});
+		
+
+        $.fn.validate = function(options){
+        	var settings = $.extend({
+                        pattern : "tutaj regex" //tutaj rozszrzam options
+                }, options);
+                
+            var pattern = new RegExp(settings.pattern);
+                
+            return this.each(function() {
+                
+                
+                
+                if (pattern.test($(this).val())){
+                	
+                }else{
+                	
+                }
+                // tutaj zwaliduj addValidation($(this),validationF);
+            });
+        };
